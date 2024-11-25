9d056e190ce38aa2301032595ed0661697b68d34
Super Star <39490679+MukeshSagarZkr@users.noreply.github.com>
2024-11-25 20:04:17 +0530

    Add files via upload

---
+++ MAUI-Crud/MauiCrudUsingApi/Utility/Data/Service/Interfaces/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using TAndCal.Model;
+
+namespace TAndCal.Utility.Data.Service.Interface
+{
+    public interface IStudentService
+    {
+		Task<int> Insert(StudentModel uiPageTypeModel);
+        Task<List<StudentModel>> Get();
+		Task<StudentModel> Get(int id);
+		Task<bool> Update(StudentModel uiPageTypeModel);
+        Task<bool> Delete(int id);
+    }
+}
